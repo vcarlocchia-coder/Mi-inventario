04a752855bd8448e5ed63e5d59fea7387419f33f
vcarlocchia-coder <vcarlocchia@quilmestolosa.com>
2026-07-22 12:35:19 -0300

    NEON

--- index.ts
+++ index.ts
@@ -2,5 +2,7 @@ import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from './schema.js'
 
-const sql = neon(process.env.DATABASE_URL!)
+const connectionString = 'postgresql://neondb_owner:npg_uTOmoDNQ6r3S@ep-late-base-ach9gmhr-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+
+const sql = neon(connectionString)
 export const db = drizzle(sql, { schema })
