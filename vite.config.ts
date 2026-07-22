bbf54ccb56ce15e2954c892382a1c66de8e04131
vcarlocchia-coder <vcarlocchia@quilmestolosa.com>
2026-07-22 13:54:17 -0300

    final

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,11 @@ const config = defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      server: {
+        preset: 'vercel',
+      },
+    }),
     viteReact(),
   ],
 })
