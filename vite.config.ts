cb887ae4f5515d7aece2b4830c283a37c3c48f39
vcarlocchia-coder <vcarlocchia@quilmestolosa.com>
2026-07-22 19:26:55 -0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
-    tailwindcss(),
   ],
 })
