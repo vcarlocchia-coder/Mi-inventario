8272b21ab1de9709759167a0cb271c1f0fd25304
vcarlocchia-coder <vcarlocchia@quilmestolosa.com>
2026-07-22 12:29:25 -0300

    NEON

--- index.ts
+++ index.ts
@@ -2,6 +2,5 @@ import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from './schema.js'
 
-// process.env.DATABASE_URL es la variable donde Vercel va a inyectar tu llave secreta
 const sql = neon(process.env.DATABASE_URL!)
 export const db = drizzle(sql, { schema })
